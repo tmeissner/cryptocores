cb14f089b962bad7586e855474cd64c18ac1067d
tmeissner <programming@goodcleanfun.de>
2015-03-25 00:47:57 +0100

    add acceptin & acceptout ports

--- des/sim/verilog/tb_des.tcl
+++ des/sim/verilog/tb_des.tcl
@@ -2,9 +2,11 @@ set signals [list]
 lappend signals "tb_des.reset"
 lappend signals "tb_des.clk"
 lappend signals "tb_des.validin"
+lappend signals "tb_des.acceptout"
 lappend signals "tb_des.mode"
 lappend signals "tb_des.key"
 lappend signals "tb_des.datain"
 lappend signals "tb_des.validout"
+lappend signals "tb_des.acceptin"
 lappend signals "tb_des.dataout"
 set num_added [ gtkwave::addSignalsFromList $signals ]
