24bee6a79beadf7069bb23a27869514fa358a779
umarcor <unai.martinezcorral@ehu.eus>
2020-11-30 18:03:38 +0100

    ci: update test script

--- .github/test.sh
+++ .github/test.sh
@@ -4,14 +4,24 @@ set -e
 
 cd $(dirname "$0")/..
 
-run_sim() {
+case "$1" in
+  sim)
+    true
+  ;;
+  *)
+    echo "Unknown test suite '$1'"
+    exit 1
+  ;;
+esac
+
+run_task() {
   echo "::group::Test $1"
-  cd "$1"/sim/vhdl
-  $MAKE sim
+  cd "$1"/"$2"/vhdl
+  $MAKE "$2"
   cd ../../..
   echo '::endgroup::'
 }
 
 for item in aes cbcdes cbcmac_des cbctdes ctraes des tdes; do
-  run_sim $item
+  run_task "$item" "$1"
 done
